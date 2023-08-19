154e10a2f7fba486a95a27db368682f8325094c9
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-08-19 14:20:29 +0300

    hm-5: + gallows creation

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,25 +1,57 @@
-import { useState, useEffect } from '../../hooks/hooks';
+import { useState, useEffect, useCallback } from '../../hooks/hooks';
 import { Hangman, Keyboard, WordPattern } from './components/components';
 import { Hint, Level } from '../../components/components';
 import { Word } from '../../common/interfaces/interfaces';
+import { MAX_MISTAKES } from '../../common/constants/constants';
 import { getRandomWord } from './helpers/helpers';
 
 import styles from './styles.module.scss';
 
 const Game = () => {
-  const [mistakesNumber] = useState<number>(0);
+  const [mistakesNumber, setMistakesNumber] = useState<number>(0);
 
   const [usedLetters, setUsedLetters] = useState<string[]>([]);
 
-  const [word, setWord] = useState<Word>();
+  const [word] = useState<Word>(getRandomWord());
+
+  const isWrongLetter = useCallback((letter: string): boolean => 
+    word.title.toLowerCase().search(letter.toLowerCase()) === -1,
+  [word.title]);
+
+  const isLetterUsed = useCallback((letter: string): boolean =>
+    Boolean(usedLetters.find((lt) => lt.toLowerCase() === letter.toLowerCase())
+  ), [usedLetters]);
+
+  const isWordFilled = useCallback((): boolean => {
+    const letters = word.title.split('');
+
+    for (let i = 0; i < letters.length; i++) {
+      if (letters[i] !== ' ' && !isLetterUsed(letters[i])) {
+        return false;
+      }
+    }
+    
+    return true;
+  }, [isLetterUsed, word.title]);
+
+  const handleKeyboardClick = (letter: string) => {
+    if (isWrongLetter(letter)) {
+      setMistakesNumber(mistakesNumber + 1);
+    }
+
+    setUsedLetters([...usedLetters, letter]);
+  }
 
   useEffect(() => {
-    setWord(getRandomWord());
-  }, []);
+    if (mistakesNumber === MAX_MISTAKES) {
+      console.log("Game Over!");
+    }
+    else if (isWordFilled()) {
+      console.log("You won!");
+    }
+  }, [isWordFilled, mistakesNumber]);
 
-  const handleKeyboardClick = (letter: string) => setUsedLetters([...usedLetters, letter]); 
-
-  return word ? (
+  return (
     <div className={styles.container}>
       <Level />
 
@@ -31,9 +63,7 @@ const Game = () => {
 
       <Keyboard usedLetters={usedLetters} onClick={handleKeyboardClick} />
     </div>
-  ) : (
-    <>Loading...</> // TODO: Loading Component
-  );
+  )
 };
 
 export { Game };
