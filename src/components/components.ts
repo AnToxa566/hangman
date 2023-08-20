1596f4d33396470c2e6b2dc7b115a882b4a63dcc
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-08-20 17:36:34 +0300

    hm-8: + background audio component

--- src/components/components.ts
+++ src/components/components.ts
@@ -4,3 +4,4 @@ export { IconButton } from './icon-button/icon-button';
 export { Modal } from './modal/modal';
 export { Level } from './level/level';
 export { Hint } from './hint/hint';
+export { BackgroundAudio } from './background-audio/background-audio';
